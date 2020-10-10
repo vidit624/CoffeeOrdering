2ee924c1810aa28ebba1a2cf5eed33a6902b62a7
Saish Shenoy <saish112@gmail.com>
2020-10-10 22:01:52 +0530

    Created an Intent to send order summary directly to user via mail

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
@@ -24,7 +26,15 @@ public class MainActivity extends AppCompatActivity {
     int n = 0;
     boolean flag = false;
     public void submitOrder(View view) {
-        displayPrice(calculatePrice());
+        String s = displayPrice(calculatePrice());
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        EditText nom = (EditText) findViewById(R.id.name);
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java - coffee order summary for " + nom.getText().toString().trim());
+        intent.putExtra(Intent.EXTRA_TEXT, s);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
     public void incOrder(View view) {
         n++;
@@ -52,6 +62,9 @@ public class MainActivity extends AppCompatActivity {
             TextView quantityTextView = (TextView) findViewById(R.id.cupp);
             quantityTextView.setText("YES");
         }
+        Locale indiaLocale = new Locale("en", "IN");
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance(indiaLocale).format(calculatePrice()));
     }
     private int calculatePrice () {
         int price = 5;
@@ -60,28 +73,31 @@ public class MainActivity extends AppCompatActivity {
         CheckBox whipp = (CheckBox) findViewById(R.id.whipp);
         if (choc.isChecked()) price += 1;
         if(whipp.isChecked()) price += 1;
-        price *= n;
-        displayPrice(price);
-        return price;
+        return price*n;
     }
     private void display() {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + n);
+        Locale indiaLocale = new Locale("en", "IN");
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance(indiaLocale).format(calculatePrice()));
     }
 
-    private void displayPrice(int number){
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        if(number == 0) {
-            priceTextView.setText("Free!");
-        }
+    private String displayPrice(int number){
+        if(number == 0)
+            return "Free!";
         else {
             Locale indiaLocale = new Locale("en", "IN");
             CheckBox whipp = (CheckBox) findViewById(R.id.whipp);
             CheckBox choc = (CheckBox) findViewById(R.id.chocolate_checkbox);
             EditText nom = (EditText) findViewById(R.id.name);
-            String s = "Name: " + nom.getText().toString() + "\nAdd Whipped Cream to coffee: " + whipp.isChecked() + "\nAdd chocolate to coffee: "+ choc.isChecked() + "\nQuantity: "+ n + "\nTotal = " + NumberFormat.getCurrencyInstance(indiaLocale).format(number);
+            String s = "Name: " + nom.getText().toString();
+            if(whipp.isChecked()) s += "\nAdded Whipped Cream";
+            if(choc.isChecked()) s += "\nAdded Chocolate";
+            s += "\nQuantity: "+ n + "\nTotal = " + NumberFormat.getCurrencyInstance(indiaLocale).format(number);
             if(!flag) s += "\nThank You for saving the Environment!";
-            priceTextView.setText(s + "\nThank You, Visit Again!");
+            s += "\nThank You, Visit Again!";
+            return s;
         }
     }
 }
